0a64cc409c47f9e0780647dcabf9535c638a0db7
Aliona Kragel <aliona.kragel@mail.ru>
2024-07-18 21:40:50 +0300

    feat: add logic to modal (open/close)

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const modalOpenBtn = document.querySelector(".btn-open");
+const modalCloseBtn = document.querySelector(".btn-close");
+const modalBackground = document.querySelector(".modal-background");
+
+modalOpenBtn.addEventListener("click", function () {
+  modalBackground.style.display = "flex";
+});
+
+modalCloseBtn.addEventListener("click", function () {
+  modalBackground.style.display = "none";
+});
+
+modalBackground.addEventListener("click", function (event) {
+  if (event.target === modalBackground) {
+    modalBackground.style.display = "none";
+  }
+});
